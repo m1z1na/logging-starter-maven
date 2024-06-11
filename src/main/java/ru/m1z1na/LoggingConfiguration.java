4ddfe9dd622c62a877f4b80b66936d92d78098bd
m1z1na <51057800+m1z1na@users.noreply.github.com>
2024-06-11 07:26:24 +0300

    Init

--- src/main/java/ru/m1z1na/LoggingConfiguration.java
+++ src/main/java/ru/m1z1na/LoggingConfiguration.java
@@ -2,6 +2,6 @@ package ru.m1z1na;
 
 public class LoggingConfiguration {
     public static void testLog(String value) {
-        System.out.printf("maven - %s", value);
+        System.out.println(value);
     }
 }
